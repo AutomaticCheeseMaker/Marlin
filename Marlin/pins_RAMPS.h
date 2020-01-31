5785b7cf6497d0dc5b911fa5b13285082e675835
Thomas Preece <thomaspreece10@gmail.com>
2020-01-31 20:12:26 +0000

    feat: Moved Servos to pins 3,4,5,6 to match circuit board

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -55,21 +55,15 @@
 //
 // Servos
 //
-#ifdef IS_RAMPS_13
-  #define SERVO0_PIN        7   // RAMPS_13 // Will conflict with BTN_EN2 on LCD_I2C_VIKI
-#else
-  #define SERVO0_PIN       11
-#endif
-#define SERVO1_PIN          6
-#define SERVO2_PIN          5
-#ifndef SERVO3_PIN
-  #define SERVO3_PIN        4
-#endif
+#define SERVO0_PIN        6
+#define SERVO1_PIN        5
+#define SERVO2_PIN        4
+#define SERVO3_PIN        3
 
 //
 // Limit Switches
 //
-#define X_MIN_PIN           3
+#define X_MIN_PIN           11
 #ifndef X_MAX_PIN
   #define X_MAX_PIN         2
 #endif
